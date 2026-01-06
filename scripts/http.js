162d16c8659923a0ee2da1f23353679f472480ac
Manuel Zarat <manuel.zarat@gmail.com>
2026-01-06 04:59:53 +0100

    Update http.js

--- scripts/http.js
+++ scripts/http.js
@@ -1,3 +1,5 @@
+let io = new Console();
+
 function test1() {
 	let req = new HTTPRequest();
 	req.Method = "GET";
@@ -7,7 +9,7 @@ function test1() {
 	req.Body = "";
 	req.Encoding = "utf-8";
 
-	let body = req.Send();
+	let body = req.send();
 	if (req.error != null) {
 	  print("ERR: " + req.error + "\n");
 	} else {
@@ -53,6 +55,45 @@ function test3() {
 	}
 }
 
-test1();
-test2();
+function test4() {
+	let r = new HTTPRequest();
+	r.Method = "GET";
+	r.Url = "https://orf.at";
+
+	let resp = await (task r.send());
+	//io.print(resp);
+	io.print(r.ResponseText);
+	io.print(r.Status);
+}
+
+function test5() {
+	let r = new HTTPRequest();
+	r.Method = "GET";
+	r.Url = "https://orf.at";
+
+	let t = task {
+	  let meta = r.openstream("read");   // Response meta (ohne Body)
+	  let out = "";
+	  while (true) {
+		let chunk = r.readstream(8192);
+		if (chunk == null) break;
+		out += chunk;
+		yield; // freiwillig, damit andere Tasks drankommen
+	  }
+	  r.closestream(); // schließt
+	  return { meta: meta, body: out };
+	};
+
+	let res = await t;
+	print(res);
+}
+
+test1(); 
+io.read();
+test2(); 
+io.read();
 test3();
+io.read();
+test4();
+io.read();
+test5();
