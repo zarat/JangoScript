4ba3ef59abfa8a7b02fb9d7ac684031a56c36d1f
Manuel Zarat <manuel.zarat@gmail.com>
2026-01-06 09:58:15 +0100

    Update process.js

--- scripts/process.js
+++ scripts/process.js
@@ -1,5 +1,29 @@
-print("=== Process Poll + Task/Await/Yield Test (WORKING) ===");
+let io = new Console();
+
 setTaskLimit(4);
+
+function default() {
+	let p = new Process();
+	p.FileName = "ping";
+	p.Arguments = "1.1.1.1 -n 3";
+	p.start();
+
+	print("läuft weiter sofort…");
+}
+
+function waitForResult() {
+	let p = new Process();
+	p.FileName = "ping";
+	p.Arguments = "1.1.1.1 -n 3";
+	p.start();
+
+	while (p.isRunning()) {
+	  let s = p.read();      // non-blocking: "" wenn nix da
+	  if (s != "") print(s);
+	  yield;                 // wichtig: CPU abgeben
+	}
+	print("exit=" + p.exitCode());
+}
 
 function log(tag, msg) {
   lock("stdout") { print("[" + tag + "] " + msg); }
@@ -49,20 +73,28 @@ function runPing(tag, ip) {
   return { out: out, code: code };
 }
 
-let t1 = task { return runPing("P1", "1.1.1.1"); };
-let t2 = task { return runPing("P2", "8.8.8.8"); };
+function parallel() {
+	let t1 = task { return runPing("P1", "1.1.1.1"); };
+	let t2 = task { return runPing("P2", "8.8.8.8"); };
 
-let tick = 0;
-while (t1.status != "Done" || t2.status != "Done") {
-  tick++;
-  lock("stdout") { print("[MAIN] tick " + tick + "  t1=" + t1.status + "  t2=" + t2.status); }
-  yield;
+	let tick = 0;
+	while (t1.status != "Done" || t2.status != "Done") {
+	  tick++;
+	  lock("stdout") { print("[MAIN] tick " + tick + "  t1=" + t1.status + "  t2=" + t2.status); }
+	  yield;
+	}
+
+	let r1 = await t1;
+	let r2 = await t2;
+
+	print("=== SUMMARY ===");
+	print("P1 exit=" + r1.code + " len=" + r1.out.length);
+	print("P2 exit=" + r2.code + " len=" + r2.out.length);
+	print("=== END ===");
 }
 
-let r1 = await t1;
-let r2 = await t2;
-
-print("=== SUMMARY ===");
-print("P1 exit=" + r1.code + " len=" + r1.out.length);
-print("P2 exit=" + r2.code + " len=" + r2.out.length);
-print("=== END ===");
+default();
+io.read();
+waitForResult();
+io.read();
+parallel();
